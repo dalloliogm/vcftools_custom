29501cf782f1a2ea583e9beab9c4996ce3ded2cf
Giovanni DallOlio <dalloliogm@gmail.com>
2015-06-04 09:13:46 +0100

    CLEAN: removed .d files

--- cpp/bcf_entry.d
+++ cpp/bcf_entry.d
@@ -1,2 +1,2 @@
 bcf_entry.o: bcf_entry.cpp bcf_entry.h output_log.h entry.h header.h \
- bgzf.h parameters.h
+  bgzf.h parameters.h
--- cpp/vcf_entry_setters.d
+++ cpp/vcf_entry_setters.d
@@ -1,2 +1,2 @@
 vcf_entry_setters.o: vcf_entry_setters.cpp vcf_entry.h entry.h header.h \
- output_log.h bgzf.h parameters.h
+  output_log.h bgzf.h parameters.h
